68190095fa28b9133974253b7a40287de3373fd4
sprout <krsproutbro@gmail.com>
2022-04-24 22:03:03 +0900

    찜 도메인, 컨트롤러 수정

--- src/main/java/com/spring/baemin/domain/Wish.java
+++ src/main/java/com/spring/baemin/domain/Wish.java
@@ -1,5 +1,31 @@
 package com.spring.baemin.domain;
 
 public class Wish {
+    private Long wish_no;
+    private String user_id;
+    private Long store_no;
 
+    public Long getWish_no() {
+        return wish_no;
+    }
+
+    public void setWish_no(Long wish_no) {
+        this.wish_no = wish_no;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public Long getStore_no() {
+        return store_no;
+    }
+
+    public void setStore_no(Long store_no) {
+        this.store_no = store_no;
+    }
 }
